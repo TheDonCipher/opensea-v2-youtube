dad1f2f3251b37774b429ea2bc8816af17794937
Eugene <eugeniusratshipa@gmail.com>
2022-09-02 09:58:34 +0000

    Added NFT Cards

--- components/Home/Listings.js
+++ components/Home/Listings.js
@@ -1,28 +1,52 @@
 import { useEffect, useState } from 'react'
-import Link from 'next/Link'
+import Link from 'next/link'
 import { useMarketplace } from '@thirdweb-dev/react'
+import NFTCard from './NFTCard'
+
+const style = {
+  wrapper: 'mx-auto grid max-w-fit flex-1 grid-cols-1 gap-8 p-10 pt-24 md:grid-cols-2 md:pt-0 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5'
+}
 
 const Listings = () => {
-
   const [listings,setListings] = useState([])
-  //const marketplace = useMaketplace()
+  const marketplace = useMarketplace("0xb9628Cd3E4Dfcb9382246787f3a60Ba18dF6a812") 
 
 
+  
   useEffect(() => {
     getListings()
   }, [])
   
   const getListings = async () => {
     try {
+      const list = await marketplace.getActiveListings()
       
+      setListings(list)
     } catch (error) {
-      
+      console.log(error)
     }
   }
   
   return (
-    <div>
-    Listings
+    <div className = {style.wrapper}>
+      {listings.length > 0 ? (
+      <>
+        {listings?.map((listing, index)=>(
+        <Link
+          key = {index}
+          href = {`/assets/${listing.assetContractAddress}/${listing.id}`}
+          
+          >
+          <a>
+            <NFTCard listing = {listing}/>
+          </a>
+          </Link>
+        
+        ))}
+      </>
+      ) : (
+        <div>Loading...</div>
+      )} 
     </div>
   )
 }
